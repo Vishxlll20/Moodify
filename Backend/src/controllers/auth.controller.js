9d244f9b921ce99b7b91dbcc01c014c5983deb82
Vishxlll20 <vishalk785032@gmail.com>
2026-03-03 21:13:03 +0530

    Auth

---
+++ Backend/src/controllers/auth.controller.js
@@ -0,0 +1,87 @@
+const express = require("express")
+const userModel = require("../models/user.model")
+const bcrypt = require("bcryptjs")
+const jwt = require("jsonwebtoken")
+
+async function register(req,res){
+    const {username,email,password} = req.body
+
+    const userExists = await userModel.findOne({
+        $or:[
+            {email},
+            {username}
+    ]
+    })
+
+    if(userExists){
+        res.status(409).json({
+            message : "User already Exists"
+        })
+    }
+
+    const hash = await bcrypt.hash(password,10);
+
+    const user = await userModel.create({
+        username,
+        email,
+        password: hash
+    })
+
+    const token = jwt.sign({
+        id:user._id,
+        username:user.username
+    },process.env.JWT_SECRET,{
+        expiresIn :"1d",
+    })
+
+    res.cookie("token",token)
+
+    res.status(201).json({
+        message:"User register Successfully",
+        user
+    })
+}
+
+async function login(req,res){
+    const {username,email,password} = req.body;
+    
+    const user = await userModel.findOne({
+        $or:[
+            {email},
+            {username}
+        ]
+    })
+
+    if(!user){
+        res.status(409).json({
+            message : "User does not exist"
+        })
+    }
+
+    const isPassValid = await bcrypt.compare(password,user.password)
+
+    if(!isPassValid){
+        res.status(409).json({
+            message : "Invalid Credentials"
+        })
+    }
+
+    const token = jwt.sign({
+        id:user._id,
+        username:user.username
+    },process.env.JWT_SECRET,{
+        expiresIn :"1d",
+    })
+
+    res.cookie("token",token)
+
+    res.status(209).json({
+        message : "User logged IN",
+        user
+    })
+}
+
+module.exports = {
+    register,
+    login
+}
---
+++ Backend/src/models/user.model.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose")
+
+const userSchema = new mongoose.Schema({
+    username:{
+        type:String,
+        required:[true,"Username is required"],
+        unique:[true,"Try another username"]
+    },
+    username:{
+        type:String,
+        required:[true,"email is required"],
+        unique:[true,"email already exists"]
+    },
+    password:{
+        type:String,
+        required:[true,"Username is required"],
+
+    }
+})
+
+const userModel = mongoose.model("users",userSchema)
+
+module.exports = userModel
