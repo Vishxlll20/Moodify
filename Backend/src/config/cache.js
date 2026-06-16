b33094269edc3074de848d34cc004671a4d36db9
Vishxlll20 <vishalk785032@gmail.com>
2026-06-16 17:35:12 +0530

    updated Redis config

--- Backend/src/config/cache.js
+++ Backend/src/config/cache.js
@@ -1,17 +1,8 @@
 const Redis = require("ioredis");
 
-const redis = new Redis({
-    host: process.env.REDIS_HOST,
-    port: process.env.REDIS_PORT,
-    password: process.env.REDIS_PASSWORD,
-});
+const redis = new Redis(process.env.REDIS_URL);
 
-redis.on("connect", () => {
-    console.log("Connected to Redis");
-});
-
-redis.on("error", (err) => {
-    console.error("Redis error:", err);
-});
+redis.on("connect", () => console.log("Connected to Redis"));
+redis.on("error", (err) => console.error("Redis error:", err));
 
 module.exports = redis;
